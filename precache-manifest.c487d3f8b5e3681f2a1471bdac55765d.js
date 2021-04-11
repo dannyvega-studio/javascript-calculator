7812cd96e44c426b85bdfadb192a8460bb7b548e
Danny Vega <67448064+dannyvega-studio@users.noreply.github.com>
2021-04-11 13:38:15 -0600

    Updates

---
+++ precache-manifest.c487d3f8b5e3681f2a1471bdac55765d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39dcd3113c92ec100b9798f0c26a3f18",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "504fb39a65c2b914bf83",
+    "url": "/javascript-calculator/static/css/main.b31209ab.chunk.css"
+  },
+  {
+    "revision": "751c2bb685437a34d9da",
+    "url": "/javascript-calculator/static/js/2.31d8ed29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-calculator/static/js/2.31d8ed29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504fb39a65c2b914bf83",
+    "url": "/javascript-calculator/static/js/main.750678cd.chunk.js"
+  },
+  {
+    "revision": "a7e4e945bd30d23581eb",
+    "url": "/javascript-calculator/static/js/runtime-main.badca6a8.js"
+  }
+]);
